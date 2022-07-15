9db2e5269287c42ffa534ffd3c1b42045a224cc8
wszgrcy <wszgrcy@gmail.com>
2022-07-16 07:43:27 +0800

    1.2.2

---
+++ src/components/component1/component1.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component1',
+  templateUrl: './component1.component.html',
+  standalone: true,
+})
+export class Component1Component implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/pages/page1/page1.module.ts
+++ src/pages/page1/page1.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from 'angular-miniprogram/common';
 import { Page1Component } from './page1.component';
 import { FirstModule } from 'first';
+import { Component1Component } from '../../components/component1/component1.component';
 @NgModule({
   declarations: [Page1Component],
-  imports: [CommonModule, FirstModule],
+  imports: [CommonModule, FirstModule, Component1Component],
   providers: [],
   bootstrap: [],
   exports: [Page1Component],
